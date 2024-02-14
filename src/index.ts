f8bb23be644713a7654a0d21fbe3f9e183e44c8d
ipqw <iga538741@gmail.com>
2024-02-14 16:53:12 +0400

    import path fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import { router } from './src/routes';
+import { router } from './routes';
 import sequelize from './db';
 
 const PORT = process.env.PORT || 5000;
