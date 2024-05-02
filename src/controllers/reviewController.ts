264bfb7a7229e07bfd9b25cd3ef7ca8475ff7e14
ipqw <iga538741@gmail.com>
2024-05-02 22:44:24 +0400

    fix

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -23,18 +23,18 @@ class reviewController {
     };
     getAllReviews = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const { productGroupId, limit = 5 } = req.body;
-            if (productGroupId) {
+            const { productGroupId, limit = 5 } = req.query;
+            if (productGroupId && Number(productGroupId) !== 0) {
                 const reviews = await Review.findAll({
                     include: { model: Like, as: 'likes' },
-                    where: { productGroupId },
-                    limit,
+                    where: { productGroupId: Number(productGroupId) },
+                    limit: Number(limit),
                 });
                 return res.json(reviews);
             }
             const reviews = await Review.findAll({
                 include: { model: Like, as: 'likes' },
-                limit,
+                limit: Number(limit),
             });
             return res.json(reviews);
         } catch (error) {
